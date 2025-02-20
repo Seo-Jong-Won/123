6a3a3c22501dd456fadcad231f72702169b83a21
lmn030200@gmail.com <opqr585000>
2025-02-20 17:00:01 +0900

    first commit

---
+++ src/ArithmetiDemo.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class ArithmetiDemo {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in)
+    System.out.print("정수를 입력해주세요 : ");
+    int number = in.nextInt();
+    int remainder = number % 2;
+    if (remainder == 0) {
+
+
+    int a = 1_000_000;
+    int b = 2_000_000;
+
+    long result = (long) a * b;
+    System.out.println(result);
+
+    int apple = 1;
+    double priceUnit = 0.1;
+    int number = 7;
+
+    double result2 = number * priceUnit;
+    System.out.println(result2);
+
+    Scanner in = new Scanner(System.in);
+    System.out.print("어떤 숫자로 나눌까요 (ex- 0.1 과 같은 실수)? " );
+    double i = in.nextDouble();
+    System.out.println(3 / i + 100);
+    System.out.println(3 % i + 100);
+
+    if (true) {
+      int a = 1;
+
+    }
+
+  }
+}
---
+++ src/ScopeVarDemo.java
@@ -0,0 +1,23 @@
+public class ScopeVarDemo {
+  public static void main(String[] args) {
+    int var1 = 1;
+
+    if (true) {
+      int var2 = 2;
+      System.out.println("var1  ==>  " + var1);
+      System.out.println("var2   ==>  " + var2);
+    }
+
+    for (String arg : args) {
+      int var3 = 3;
+      System.out.println("var1  ==>  " + var1);
+      //System.out.println("var2  ==>  " + var2);
+      System.out.println("(for)  var3  ==>" + var3);
+    }
+
+    System.out.println("(main)  var1  ==>" + var1);
+    System.out.println("(main) var1  ==>");
+
+  }
+}
+
